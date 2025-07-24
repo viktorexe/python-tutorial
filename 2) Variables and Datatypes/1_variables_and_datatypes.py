c89071233384cd58a05f2b038b5035b887beb0a0
viktorexe <developer.viktorexe@gmail.com>
2025-07-24 22:59:31 +0530

    Create 1_variables_and_datatypes.py
    Co-Authored-By: Vansh Sharma <sharma.vansh7273@gmail.com>

---
+++ 2) Variables and Datatypes/1_variables_and_datatypes.py
@@ -0,0 +1,34 @@
+# A variable is like a container that stores data. You give it a name and assign a value to it.
+
+name = "Guido Van Rossum"
+age = 18
+
+'''
+name is the variable.
+= means “store this in”.
+"Guido Van Rossum" is the value we stored.
+'''
+
+'''
+Datatype means the type of value 
+being stored inside the variable
+'''
+
+'''Types of data'''
+# Text
+username = "Vansh"
+
+# Whole number
+userage = 18
+
+# Decimal number
+height = 5.9
+
+# True or False
+is_student = True
+
+# A list of items
+fruits = ["apple", "banana", "mango"]
+
+# To check what type of data is being stored inside a variable we use
+print(type(name))
