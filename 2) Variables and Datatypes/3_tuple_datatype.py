2aa512dfe909a17c7a0eee18a909c97def5360bc
viktorexe <developer.viktorexe@gmail.com>
2025-07-24 23:09:37 +0530

    variables and datatypes
    Co-Authored-By: Vansh Sharma <sharma.vansh7273@gmail.com>

---
+++ 2) Variables and Datatypes/3_tuple_datatype.py
@@ -0,0 +1,11 @@
+'''
+Tuple and lists are almost same but difference is that
+- Tuples cannto be changed (are immutable)
+- Instead of [] we use () in tuples
+- It is considered fast and secure
+'''
+
+tuple_1 = (11, 'name', 2.2)
+print(tuple_1[0])
+print(tuple_1[1])
+print(tuple_1[2])
