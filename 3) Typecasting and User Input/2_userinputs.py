51bb5c9f430f390b6300fcb8ab9e5839e9f03e0b
viktorexe <developer.viktorexe@gmail.com>
2025-07-24 23:44:42 +0530

    Typecasting and Inputs
    Co-Authored-By: Vansh Sharma <sharma.vansh7273@gmail.com>

---
+++ 3) Typecasting and User Input/2_userinputs.py
@@ -0,0 +1,27 @@
+'''
+Inputs are used to take a value as an input from the user
+You cannot add integer to someone input which is of string
+Integers can be added to integer inputs only 
+'''
+
+# 1. String Inputs
+name = input('Please enter your name: ')
+print('Your name is:', name )
+
+# 2. Integer input
+age = int(input('Please enter your age: '))
+print('Your age is:', age)
+
+# 3. Float Input
+height = float(input('Please enter your height (in feet): '))
+print('You height is:', height,'feet')
+
+# 4. Adding int to integer inputs
+current_age = int(input("Please enter your current age: "))
+print('Your age next year will be: ', current_age + 1)
+
+# 5. To take two values in one input at once
+value1, value2 = input('Please enter 2 values you want to add: ').split()
+value1 = int(value1)
+value2 = int(value2)
+print(value1 + value2)
