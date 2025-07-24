8a13874f77794cbc9797400637d8618f3c5d1b2b
viktorexe <developer.viktorexe@gmail.com>
2025-07-24 23:05:47 +0530

    Update 1.1_list_datatypes.py
    Co-Authored-By: Vansh Sharma <sharma.vansh7273@gmail.com>

---
+++ 2) Variables and Datatypes/1.1_list_datatypes.py
@@ -0,0 +1,19 @@
+# List is like a shopping list in which we can store multiple items together 
+
+my_list = ['apple', 'banana'] # Square brackets are used 
+
+# We can add/remove/change the elements of a list
+
+my_list2 = [11, 11.1, 232, 'apple', 'banana'] # Multiple datatypes can be stored in a list
+
+'''
+In a list the first element has 
+the serial number 0
+for example in my_list the 'apple' element has 
+0 as serial number and we can replace or print it 
+'''
+
+print(my_list[0]) # prints apple
+my_list[0] = 'orange' # replaced apple with orange 
+print(my_list) # Will print new list with orange in place of apple 
+
