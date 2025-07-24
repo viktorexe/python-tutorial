795ac36f47061bc04a38587f99f4511298414c7c
viktorexe <developer.viktorexe@gmail.com>
2025-07-24 23:31:43 +0530

    Update 1_basics.py
    Co-Authored-By: Vansh Sharma <sharma.vansh7273@gmail.com>

--- 3) Typecasting/1_basics.py
+++ 3) Typecasting/1_basics.py
@@ -3,3 +3,26 @@ Typecasting means to convert one datatype to other
 string to integer
 interger to string
 '''
+
+a = '99'
+b = int(a)
+print(b + 10)
+
+x = 99
+y = str(x)
+print("Value is "+ y)
+
+f = 5.9
+i = int(f)
+print(i)    # Output: 5 (decimal hata diya)
+
+print(bool(0))       # False
+print(bool(10))      # True
+print(bool(""))      # False
+print(bool("hello")) # True
+
+# NOTE - Boolean returns True if something exist and false of there is nothing or 0
+'''
+x = "abc"
+y = int(x)     # ❌ Error: because "abc" is not a number
+'''
