6ea3b71091bd20b083c87c22ed0183109f822eb1
viktorexe <developer.viktorexe@gmail.com>
2025-07-30 17:02:00 +0530

    Update 3_string_operations.py

--- 4) Strings/3_string_operations.py
+++ 4) Strings/3_string_operations.py
@@ -1,6 +1,10 @@
 # Strings are immutable, existing strings never change, it makes a new string
 
-a = 'Mango'
+a = 'mango'
+b = 'Banana!!!'
 print(len(a)) # Prints Length of a
 print(a.upper()) # Fully capatilizes the string
-print(a.lower())
+print(a.lower()) # Changes to string to lowercase
+print(a.capatilize()) # Converts everything to lowercase and only capitalizes the first letter of the string
+print(a.replace('Mango', 'Apple')) # Replaces the character(s) in the string and returns a modified string 
+print(b.rstrip('!')) # Removes unwanted characters from strings (does not work for the ones before string)
