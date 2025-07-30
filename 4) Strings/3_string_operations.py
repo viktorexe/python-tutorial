9477c31fb22e15e4c1bf3a0428a4b73ab54354ba
viktorexe <developer.viktorexe@gmail.com>
2025-07-30 17:17:23 +0530

    Update 3_string_operations.py

--- 4) Strings/3_string_operations.py
+++ 4) Strings/3_string_operations.py
@@ -15,10 +15,11 @@ print(a.replace('Mango', 'Apple')) # Replaces the character(s) in the string and
 print(a.isalpha()) # Returns True if the whole string is only A-Z or a-z, no white spaces, numbers or characters
 print(a.islower()) # Returns True if the whole string is lowercase
 print(a.isprintable()) # Returns True if everything in the string is printable
+print(a.swapcase()) # Converts lowercase to uppercase and vice-versa 
 print(b.rstrip('!')) # Removes unwanted characters from strings (does not work for the ones before string)
 print(c.rstrip('!'))
 print(c.endswith('!')) # To check if a given string ends with the entered character (returns True or False)
-print(c.endswith('Welcome')) # To check if a given string starts with the entered character (returns True or False)
+print(c.startswith('Welcome')) # To check if a given string starts with the entered character (returns True or False)
 print(c.center(50)) # Shifts the text to centre with 50 blank spaces on left
 print(d.count('hello')) # Counts the numebr of time the thing appears in the string
 print(d.find('you')) # Finds the thing and returns with index number, if not found returns -1
