a22c129af3d676f45c7543444b96f3437315428b
viktorexe <developer.viktorexe@gmail.com>
2025-07-30 12:06:33 +0530

    Update 1_basics.py

--- 4) Strings/1_basics.py
+++ 4) Strings/1_basics.py
@@ -5,4 +5,9 @@ A string is a group of letters / words or sentences
 name = 'Viktor'
 country = 'India'
 
-# To 
+# To print the first letter of country or all one by one
+print(country[0])
+print(country[1])
+print(country[2])
+print(country[3])
+print(country[4])
