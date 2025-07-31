5ec3877983bb99695fccde088419a0f9ba08c970
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:16:11 +0530

    Update 1_nested.py

--- 6) Nested Loops/1_nested.py
+++ 6) Nested Loops/1_nested.py
@@ -1,2 +1,8 @@
 # Nested Loops
-# In this we will take a number as input and print statements 
+# In this we will take a number as input and print statements on basis of it
+
+num = int(input('Please enter a number: '))
+if(num<0):
+    print('Entered number is negative')
+elif(num>0):
+    if(num>1 )
