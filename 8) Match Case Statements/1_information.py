0aa5f9e50e95d3309da99600ea9cd6336ec50bed
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 13:36:51 +0530

    Update 1_information.py

--- 8) Match Case Statements/1_information.py
+++ 8) Match Case Statements/1_information.py
@@ -1,3 +1,7 @@
 '''
 In other languages like c, java, there is switch case. Similarly in python we have match case
+case _: means default case, we can make multiple default cases like
+case_:
+case_ if x!= 90:
+
 '''
