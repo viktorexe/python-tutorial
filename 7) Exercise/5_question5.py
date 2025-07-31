6175db7ce4d6fd95b137701d3e36bc37b99cd2e6
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:44:11 +0530

    Update 5_question5.py

--- 7) Exercise/5_question5.py
+++ 7) Exercise/5_question5.py
@@ -1,5 +1,5 @@
 '''
-5. Digital Clock Style Greeting
+Digital Clock Style Greeting
 Input: Hour (0 to 23)
 Output:
 0-11: "Good Morning!"
@@ -8,3 +8,5 @@ Output:
 21-23: "Good Night!"
 ⚠️ Invalid hour → "Invalid time!"
 '''
+
+time = int(input('Please enter the time between 0-23 hrs: '))
