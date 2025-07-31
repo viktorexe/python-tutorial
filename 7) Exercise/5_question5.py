cac3486bda28e4cce94cdca92bcfaddb2bc84fff
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:43:26 +0530

    Update 5_question5.py

---
+++ 7) Exercise/5_question5.py
@@ -0,0 +1,14 @@
+'''
+5. Digital Clock Style Greeting
+🔹 Input: Hour (0 to 23)
+🔹 Output:
+
+0–11: "Good Morning!"
+
+12–16: "Good Afternoon!"
+
+17–20: "Good Evening!"
+
+21–23: "Good Night!"
+⚠️ Invalid hour → "Invalid time!"
+'''
