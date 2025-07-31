1be3819f0c016e0ca454f94323e5bff67bcf5719
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:34:20 +0530

    Update 3_question3.py

--- 7) Exercise/3_question3.py
+++ 7) Exercise/3_question3.py
@@ -1,7 +1,18 @@
 '''
-Name & Age Checker (nested if)
+Name & Age Checker
 Input: Name and Age
 If age >= 18, print "Hi [Name], you are an adult"
 Else if age < 10, print "Hello little [Name]!"
 Else "Hey [Name], you are a teenager"
 '''
+
+print('Name & Age Checker')
+name = input('Please enter your full name: ')
+age = int(input('Please enter your age: '))
+
+if(age>=18):
+    print('Hi', name, 'you are an adult')
+elif(age<10):
+    print('Hello little', name)
+else:
+    print('Hello', name, 'you are a teenager')
