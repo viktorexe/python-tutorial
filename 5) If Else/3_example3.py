20a13e7825ac566478cc5119c08c241443a10abc
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:09:40 +0530

    Update 3_example3.py

--- 5) If Else/3_example3.py
+++ 5) If Else/3_example3.py
@@ -4,3 +4,7 @@ num = int(input('Please enter a number: ')) # Taking user input for a number
 
 if(num>0):
     print(num,'is a positive number and greater than 0')
+elif(num<0):
+    print(num,'is a negative number and less than 0')
+else:
+    print(num,'is 0, neither positive nor negative')
