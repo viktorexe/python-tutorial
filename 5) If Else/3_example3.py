31fd2a5cbe336613690d0cc2a0428875c9dc0a74
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:08:54 +0530

    Update 3_example3.py

--- 5) If Else/3_example3.py
+++ 5) If Else/3_example3.py
@@ -1,3 +1,6 @@
 # Type of integer, using elif
 
-num = int(input('Please enter a number: '))
+num = int(input('Please enter a number: ')) # Taking user input for a number
+
+if(num>0):
+    print(num,'is a positive number and greater than 0')
