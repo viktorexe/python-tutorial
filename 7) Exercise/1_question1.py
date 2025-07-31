5d4b1c3260a922c5e290d158414d5bb40dc475e8
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:22:22 +0530

    Exercises

---
+++ 7) Exercise/1_question1.py
@@ -0,0 +1,10 @@
+'''
+Temperature Checker
+Input: A number (temperature)
+Task: Print:
+"Cold Day ❄️" if below 15
+"Pleasant 🌤️" if 15-25
+"Hot 🔥" if above 25
+'''
+
+print('Temperature Checker program!')
---
+++ 7) Exercise/2_question2.py
@@ -0,0 +1,9 @@
+'''
+Grade Calculator (using elif)
+Input: Marks (0-100)
+Output:
+90-100 → A
+70-89 → B
+50-69 → C
+<50 → Fail
+'''
