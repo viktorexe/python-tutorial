a5c04a451d4bd33116eb60d200edfeb6dfc0040d
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 13:19:02 +0530

    Update 1_question1.py

--- 9) Exercise (Match Case)/1_question1.py
+++ 9) Exercise (Match Case)/1_question1.py
@@ -3,3 +3,5 @@
 operator = input('Please enter the operation to perform: +, -, *, /')
 match operator: # Match statement for operator 
     case '+':
+        num1 = int(input('Please enter first number: '))
+        num1 = int(input('Please enter  number: '))
