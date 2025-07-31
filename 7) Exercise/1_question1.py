c091b0d09258b72b05486c9273e82049641d1264
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:25:59 +0530

    Update 1_question1.py

--- 7) Exercise/1_question1.py
+++ 7) Exercise/1_question1.py
@@ -2,10 +2,16 @@
 Temperature Checker
 Input: A number (temperature)
 Task: Print:
-"Cold Day ❄️" if below 15
-"Pleasant 🌤️" if 15-25
-"Hot 🔥" if above 25
+"Cold Day" if below 15
+"Pleasant" if 15-25
+"Hot" if above 25
 '''
 
 print('Temperature Checker program!') 
 temperature = float(input('Enter the temperature: ')) # Float input for temperature as it is decimal  
+if(temperature<15):
+    print('Cold Day')
+elif(temperature>=15 and temperature<=25):
+    print('Pleasant')
+elif(temperature>=25):
+    print('Hot')
