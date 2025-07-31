c6dc48ea1a1fe33ad8909bd556c8e7e62cbd18df
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:51:12 +0530

    Update 6_question6.py

--- 7) Exercise/6_question6.py
+++ 7) Exercise/6_question6.py
@@ -10,9 +10,4 @@ Length > 10 and mix → "Very Strong"
 '''
 
 password = input('Please enter your password: ')
-if(password==int):
-    print('Your password is weak and has only numbers:')
-    if(password == int and password.isalphanum()):
-        print('Your password is medium strength and has numbers and letters')
-else:
-    print('idk')
+if(password.isdigit()):
