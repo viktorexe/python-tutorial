980eeab1bc2e1ca1fec6729d7b6a73c85d0c5454
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:48:50 +0530

    Update 6_question6.py

--- 7) Exercise/6_question6.py
+++ 7) Exercise/6_question6.py
@@ -1,3 +1,11 @@
 '''
-
+Bonus Challenge (Mini Project Idea)
+Basic Password Strength Checker
+Input: password
+Check:
+Length < 6 → "Very Weak"
+Only letters or only digits → "Weak"
+Letters + digits → "Strong"
+Length > 10 and mix → "Very Strong"
 '''
+
