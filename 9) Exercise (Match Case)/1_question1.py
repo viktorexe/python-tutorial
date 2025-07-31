668c069e66561caa898a7e589ecb62230bea7881
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 13:20:22 +0530

    Update 1_question1.py

--- 9) Exercise (Match Case)/1_question1.py
+++ 9) Exercise (Match Case)/1_question1.py
@@ -3,7 +3,9 @@
 operator = input('Please enter the operation to perform: +, -, *, /')
 match operator: # Match statement for operator 
     case '+':
-        num1 = int(input('Please enter first number: '))
-        num2 = int(input('Please enter second number: '))
-        sum = num1 + num2
+        num1_add = int(input('Please enter first number: '))
+        num2_add = int(input('Please enter second number: '))
+        sum = num1_add + num2_add
         print('The sum is: ',sum)
+    case '-':
+        num1
