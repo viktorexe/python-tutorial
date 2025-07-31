ded5bd6bb1cda6b18d39783562d516b810120c27
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 13:31:20 +0530

    Update 2_question2.py

--- 9) Exercise (Match Case)/2_question2.py
+++ 9) Exercise (Match Case)/2_question2.py
@@ -8,3 +8,10 @@ anything else → "Invalid color"
 '''
 
 color = input('please enter the color: ')
+
+match color: # Match statement for color
+    
+    case 'red': # If the user enters red in the input
+        print('Red light means stop')
+
+    case 'yellow': # If the user enters yellow in the input
