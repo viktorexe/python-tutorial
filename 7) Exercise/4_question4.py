856c600ab49997e408127cc837c5153c7ce63366
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:36:26 +0530

    Update 4_question4.py

--- 7) Exercise/4_question4.py
+++ 7) Exercise/4_question4.py
@@ -1,3 +1,13 @@
 '''
+4. Odd or Even + Multiple of 5
+Input: A number
+Print:
 
+"Even and multiple of 5"
+
+"Odd and multiple of 5"
+
+"Even but not multiple of 5"
+
+"Odd and not multiple of 5"
 '''
