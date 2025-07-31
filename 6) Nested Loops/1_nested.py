56dba08a48857f3ded1a1b045b432e048ac5d588
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 12:17:27 +0530

    Update 1_nested.py

--- 6) Nested Loops/1_nested.py
+++ 6) Nested Loops/1_nested.py
@@ -5,4 +5,9 @@ num = int(input('Please enter a number: '))
 if(num<0):
     print('Entered number is negative')
 elif(num>0):
-    if(num>1 )
+    if(num >= 1 and num <= 5):
+        print('Entered number lies between 1-5')
+    if(num>5 and num<=10):
+        print('Number lies between 6-10')
+    else:
+        print('Number is greate')
