fa6c42e787204d43102c1444286da4449aa50913
viktorexe <developer.viktorexe@gmail.com>
2025-07-31 13:29:50 +0530

    Update 2_question2.py

---
+++ 9) Exercise (Match Case)/2_question2.py
@@ -0,0 +1,10 @@
+'''
+Input: A string ("red", "yellow", "green")
+Output:
+red → "Stop"
+yellow → "Slow down"
+green → "Go"
+anything else → "Invalid color"
+'''
+
+color = input('please enter the color: ')
