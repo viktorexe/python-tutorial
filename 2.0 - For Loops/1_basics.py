06a8494497c575827a3884137b511e2db7e11537
viktorexe <developer.viktorexe@gmail.com>
2025-08-02 20:28:10 +0530

    Update 1_basics.py

--- 2.0 - For Loops/1_basics.py
+++ 2.0 - For Loops/1_basics.py
@@ -1,5 +1,7 @@
 '''
 Sometimes a programmer wants to execute a group of statements for a certain period of time 
 for which they use loops
-Loops are 
+Loops are of two types
+1. For Loop
+2. While Loop
 '''
