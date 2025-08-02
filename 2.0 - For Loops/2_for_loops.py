26ace64d9214dd46409867a70b8a05be18a86668
viktorexe <developer.viktorexe@gmail.com>
2025-08-02 20:32:10 +0530

    Update 2_for_loops.py

--- 2.0 - For Loops/2_for_loops.py
+++ 2.0 - For Loops/2_for_loops.py
@@ -4,5 +4,9 @@ for i in name:
 
 '''
 Code Explanation:
-
+for i in name: Here i is referred to as index, one by one index value shifts in the string till the end, similary different for list
 '''
+
+list = ['hi', 'hello', 12]
+for i in list:
+    print(i) # Prints every object in the list 
